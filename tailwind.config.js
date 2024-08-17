c032cfcf8640fe6cbc8c796de9ca1669bffb3584
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-17 12:06:52 +0200

    Formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          'pizza-orange': '#ff5400',
-        }
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pizza-orange': '#ff5400',
       }
-    },
-    plugins: [],
+    }
+  },
+  plugins: [],
 }
