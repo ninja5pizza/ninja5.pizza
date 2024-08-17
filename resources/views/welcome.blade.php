3d82a1a6d9be530ef5c78220dbe83ea794f3da10
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-17 11:27:52 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>NINJA5.pizzq</title>
+        <title>{{ config('app.name') }}</title>
     </head>
     <body>
 
