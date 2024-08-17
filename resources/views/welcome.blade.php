7ac59be6d2a18a9b9559011473d1393ae71b9e54
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-17 20:02:15 +0200

    Add open graph and twitter meta tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,16 @@
 
         @vite('resources/css/app.css')
 
+        <meta property="og:url" content="{{ url()->full() }}">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="{{ config('app.name') }}">
+        <meta property="og:description" content="We are the NINJA5 from the Pizza Ninjas Bootcamp 2024">
+        <meta property="og:image" content="{{ asset('images/twitter-card-large.png') }}">
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{ config('app.name') }}">
+        <meta name="twitter:image" content="{{ asset('images/twitter-card-large.png') }}">
+
         @if(config('services.fathom.site_id'))
         <script src="https://cdn.usefathom.com/script.js" data-site="{{ config('services.fathom.site_id') }}" defer></script>
         @endif
