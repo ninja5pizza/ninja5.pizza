f00a1ca67139e669e77ef4a04b79b17d1905485c
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-23 15:39:22 +0200

    Place content in center of the screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,104 +23,139 @@
         @endif
     </head>
     <body class="bg-pizza-orange">
-        <h1 class="p-16 text-3xl text-white font-bold">
-            {{ config('app.name') }}
-        </h1>
-        <div class="flex px-16 space-x-10 text-white">
-            <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
-            <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
-            <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
-            <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
-            <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
-        </div>
-        <div class="flex mt-8 px-16 space-x-10 text-white">
-            <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
-            <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
-            <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
-            <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
-            <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
-        </div>
-        <h2 class="px-16 mt-24 text-3xl text-white font-bold">
-            TEAM
-        </h2>
-        <div class="flex px-16 mt-8 space-x-10 text-white">
-            <div class="flex flex-col">
-                <a
-                    href="https://x.com/billybitcoins"
-                    class="hover:text-orange-100"
-                    target="_blank"
-                >
-                    <x-ninjas-807 class="w-24 border rounded-lg"/>
-                    <span class="mt-1 text-xs">
-                        @billybitcoins
-                    </span>
-                </a>
-            </div>
-
-            <div class="flex flex-col">
-                <a
-                    href="https://x.com/KimiBoyNFT"
-                    class="hover:text-orange-100"
-                    target="_blank"
-                >
-                    <x-ninjas-110 class="w-24 border rounded-lg"/>
-                    <span class="mt-1 text-xs">
-                        @KimiBoyNFT
-                    </span>
-                </a>
-            </div>
-
-            <div class="flex flex-col">
-                <a
-                    href="https://x.com/sjen_anoubis"
-                    class="hover:text-orange-100"
-                    target="_blank"
-                >
-                    <x-ninjas-268 class="w-24 border rounded-lg"/>
-                    <span class="mt-1 text-xs">
-                        @sjen_anoubis
-                    </span>
-                </a>
-            </div>
-
-            <div class="flex flex-col">
-                <a
-                    href="https://x.com/mca_chef"
-                    class="hover:text-orange-100"
-                    target="_blank"
-                >
-                    <x-ninjas-962 class="w-24 border rounded-lg"/>
-                    <span class="mt-1 text-xs">
-                        @mca_chef
-                    </span>
-                </a>
-            </div>
-
-            <div class="flex flex-col">
-                <a
-                    href="https://x.com/mvdnbrk"
-                    class="hover:text-orange-100"
-                    target="_blank"
-                >
-                    <x-ninjas-874 class="w-24 border rounded-lg"/>
-                    <span class="mt-1 text-xs">
-                        @mvdnbrk
-                    </span>
-                </a>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <h1 class="mt-4 text-3xl text-white font-bold">
+                    {{ config('app.name') }}
+                </h1>
             </div>
         </div>
-        <h3 class="px-16 mt-24 text-3xl text-white font-bold">
-            FOLLOW US
-        </h3>
-        <div class="flex mt-8 px-16 text-white">
-            <a
-                href="https://x.com/ninja5_pizza"
-                class="hover:text-orange-100"
-                target="_blank"
-            >
-                <x-icon-twitter-x class="w-8"/>
-            </a>
+
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <div class="mt-8 flex space-x-10 text-white">
+                    <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-ninja class="p-4 w-24 border rounded-lg"/>
+                </div>
+            </div>
         </div>
+
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <div class="flex mt-8 space-x-10 text-white">
+                    <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
+                    <x-icon-pizza-slice class="p-4 w-24 border rounded-lg"/>
+                </div>
+            </div>
+        </div>
+
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <h2 class="mt-24 text-3xl text-white font-bold">
+                    TEAM
+                </h2>
+            </div>
+        </div>
+
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <div class="flex mt-8 space-x-10 text-white">
+                    <div class="flex flex-col">
+                        <a
+                            href="https://x.com/billybitcoins"
+                            class="hover:text-orange-100"
+                            target="_blank"
+                        >
+                            <x-ninjas-807 class="w-24 border rounded-lg"/>
+                            <span class="mt-1 text-xs">
+                                @billybitcoins
+                            </span>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <a
+                            href="https://x.com/KimiBoyNFT"
+                            class="hover:text-orange-100"
+                            target="_blank"
+                        >
+                            <x-ninjas-110 class="w-24 border rounded-lg"/>
+                            <span class="mt-1 text-xs">
+                                @KimiBoyNFT
+                            </span>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <a
+                            href="https://x.com/sjen_anoubis"
+                            class="hover:text-orange-100"
+                            target="_blank"
+                        >
+                            <x-ninjas-268 class="w-24 border rounded-lg"/>
+                            <span class="mt-1 text-xs">
+                                @sjen_anoubis
+                            </span>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <a
+                            href="https://x.com/mca_chef"
+                            class="hover:text-orange-100"
+                            target="_blank"
+                        >
+                            <x-ninjas-962 class="w-24 border rounded-lg"/>
+                            <span class="mt-1 text-xs">
+                                @mca_chef
+                            </span>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <a
+                            href="https://x.com/mvdnbrk"
+                            class="hover:text-orange-100"
+                            target="_blank"
+                        >
+                            <x-ninjas-874 class="w-24 border rounded-lg"/>
+                            <span class="mt-1 text-xs">
+                                @mvdnbrk
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <h3 class="mt-24 text-3xl text-white font-bold">
+                    FOLLOW US
+                </h3>
+            </div>
+        </div>
+
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl">
+                <div class="flex mt-8 text-white">
+                    <a
+                        href="https://x.com/ninja5_pizza"
+                        class="hover:text-orange-100"
+                        target="_blank"
+                    >
+                        <x-icon-twitter-x class="w-8"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+
         <footer class="my-16 border-t">
 
         </footer>
