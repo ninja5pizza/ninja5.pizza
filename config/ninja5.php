69f3cb401c17e2de04d760faff1bc6c9257d2ff0
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-24 07:25:20 +0200

    Add ninja5 config file

---
+++ config/ninja5.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    'billybitcoins' => [
+
+        'pizza_ninjas_number' => 807,
+
+    ],
+
+    'KimiBoyNFT' => [
+
+        'pizza_ninjas_number' => 110,
+
+    ],
+
+    'mca_chef' => [
+
+        'pizza_ninjas_number' => 962,
+
+    ],
+
+    'mvdbbrk' => [
+
+        'pizza_ninjas_number' => 874,
+
+    ],
+
+    'sjen_anoubis' => [
+
+        'pizza_ninjas_number' => 268,
+
+    ],
+
+];
