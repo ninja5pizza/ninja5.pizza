c9100f32ceab45363cf8da7396be1405a7ac76fe
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-24 10:20:14 +0200

    Add inscription number

--- config/ninja5.php
+++ config/ninja5.php
@@ -6,6 +6,8 @@ return [
 
         'inscription_id' => '9d6e3f1c019acaaf89dfbb53432ee654bfa0aa7260848db5e579b5a669a83e38i2'
 
+        'inscription_number' => 56255086,
+
         'pizza_ninjas_number' => 807,
 
     ],
@@ -13,6 +15,8 @@ return [
     'KimiBoyNFT' => [
 
         'inscription_id' => '39399e536443446eda3f26ce7038178ec3b69157f38eaed19caea4614aabaf62i95',
+
+        'inscription_number' => 56246264,
 
         'pizza_ninjas_number' => 110,
 
@@ -22,6 +26,8 @@ return [
 
         'inscription_id' => 'e151d62f71ab4e9cfbe1a6ab368db482a393e2a01627b2f9c0e99106cd54cba5i5',
 
+        'inscription_number' => 56259592,
+
         'pizza_ninjas_number' => 962,
 
     ],
@@ -30,6 +36,8 @@ return [
 
         'inscription_id' => '9d6e3f1c019acaaf89dfbb53432ee654bfa0aa7260848db5e579b5a669a83e38i69',
 
+        'inscription_number' => 56255153,
+
         'pizza_ninjas_number' => 874,
 
     ],
@@ -37,6 +45,8 @@ return [
     'sjen_anoubis' => [
 
         'inscription_id' => '650fe4b0abc458836ef869906afbed7c306b1f948a4b3e9412812a7f83900033i88',
+
+        'inscription_number' => 56250796,
 
         'pizza_ninjas_number' => 268,
 
