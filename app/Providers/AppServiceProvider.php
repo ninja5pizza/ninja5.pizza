dcfde3c8229c0fb9ab0624b3a58ca2f9cd35cd9c
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-24 10:39:43 +0200

    Share ninja5 in all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\NinjaFive;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::share('ninja5', new NinjaFive(
+            config('ninja5')
+        ));
     }
 }
