e1443d971ff4bcafa5881fa25c316c4b426db7d1
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-25 12:06:51 +0200

    Use config file to render team members

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,70 +66,20 @@
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-3xl">
                 <div class="flex mt-8 space-x-10 text-white">
+                    @foreach(config('ninja5') as $key => $value)
                     <div class="flex flex-col">
                         <a
-                            href="https://x.com/billybitcoins"
+                            href="https://x.com/{{ $key }}"
                             class="hover:text-orange-100"
                             target="_blank"
                         >
-                            <x-ninjas-807 class="w-24 border rounded-lg"/>
+                            @svg('ninjas.'.$value['pizza_ninjas_number'], 'w-24 border rounded-lg')
                             <span class="mt-1 text-xs">
-                                @billybitcoins
+                                {{ $key }}
                             </span>
                         </a>
                     </div>
-
-                    <div class="flex flex-col">
-                        <a
-                            href="https://x.com/KimiBoyNFT"
-                            class="hover:text-orange-100"
-                            target="_blank"
-                        >
-                            <x-ninjas-110 class="w-24 border rounded-lg"/>
-                            <span class="mt-1 text-xs">
-                                @KimiBoyNFT
-                            </span>
-                        </a>
-                    </div>
-
-                    <div class="flex flex-col">
-                        <a
-                            href="https://x.com/sjen_anoubis"
-                            class="hover:text-orange-100"
-                            target="_blank"
-                        >
-                            <x-ninjas-268 class="w-24 border rounded-lg"/>
-                            <span class="mt-1 text-xs">
-                                @sjen_anoubis
-                            </span>
-                        </a>
-                    </div>
-
-                    <div class="flex flex-col">
-                        <a
-                            href="https://x.com/mca_chef"
-                            class="hover:text-orange-100"
-                            target="_blank"
-                        >
-                            <x-ninjas-962 class="w-24 border rounded-lg"/>
-                            <span class="mt-1 text-xs">
-                                @mca_chef
-                            </span>
-                        </a>
-                    </div>
-
-                    <div class="flex flex-col">
-                        <a
-                            href="https://x.com/mvdnbrk"
-                            class="hover:text-orange-100"
-                            target="_blank"
-                        >
-                            <x-ninjas-874 class="w-24 border rounded-lg"/>
-                            <span class="mt-1 text-xs">
-                                @mvdnbrk
-                            </span>
-                        </a>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
