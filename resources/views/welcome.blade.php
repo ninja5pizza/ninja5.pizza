c8ca9e87383072903f54a5968dd1696b30cff40e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-08-31 14:31:06 +0200

    Optimize SVG logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
         <div class="bg-gray-50">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="mx-auto max-w-3xl">
-                    <x-icon-logo title="NINJA5.pizza"/>
+                    <x-icon-logo/>
                 </div>
             </div>
         </div>
