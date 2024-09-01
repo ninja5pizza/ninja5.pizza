3ff91aa55ddece1ba6a3e3479fd50e8e55fa77e6
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-01 21:26:34 +0200

    Create route to `/collection`

---
+++ app/Http/Controllers/PizzaNinjasCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PizzaNinjasCollection extends Controller
+{
+    public function __invoke()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PizzaNinjasCollection;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/collection', PizzaNinjasCollection::class)->name('collection');
