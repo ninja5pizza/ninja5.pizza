f50ad24b1aab2c7130c6ca991be12bfeff6c4d93
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-05 13:47:23 +0200

    Schedule the ordinals stats command

--- routes/console.php
+++ routes/console.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('bitcoin:cache-price')->everyFifteenMinutes();
+Schedule::command('bitcoin:ord-collection:cache-stats')->everyFifteenMinutes();
