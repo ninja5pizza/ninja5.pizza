272643b7c1f683ba71fd40027efd5a454e7b3a11
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-19 15:00:01 +0200

    Add job and command to fetch meta data

---
+++ app/Console/Commands/FetchNinjaMetaData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\FetchMetaData;
+use App\Models\Inscription;
+use Illuminate\Console\Command;
+
+class FetchNinjaMetaData extends Command
+{
+    protected $signature = 'ninja:meta';
+
+    protected $description = 'Retrieve meta data for an inscription.';
+
+    public function handle()
+    {
+        foreach (Inscription::all() as $inscription) {
+            FetchMetaData::dispatch($inscription);
+
+            $this->info('Fetching meta data for '.$inscription->name.'.');
+        }
+    }
+}
---
+++ app/Jobs/FetchMetaData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Inscription;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class FetchMetaData implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, Queueable;
+
+    public string $url;
+
+    public $uniqueFor = 1;
+
+    public function __construct(
+        public Inscription $inscription,
+    ) {
+        $this->url = 'https://ordiscan.com/content/'.$this->inscription->inscription_id;
+    }
+
+    public function handle(): void
+    {
+        $response = Http::get($this->url);
+
+        if ($response->successful()) {
+            $json = $this->extractJsonFromHtml($response->body());
+
+            $this->inscription->meta = $json;
+
+            $this->inscription->save();
+        }
+    }
+
+    public function extractJsonFromHtml(string $html): ?string {
+        $pattern = '/Ninja\.load\((.*?)\)/s';
+
+        preg_match($pattern, $html, $matches);
+
+        if (!empty($matches[1])) {
+            $jsonString = trim($matches[1], " \t\n\r\0\x0B,");
+
+            return $jsonString;
+        }
+
+        return null;
+    }
+}
