115dff291f737cd971da81ce34b497f17ae65f73
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-23 20:35:09 +0200

    Add twitter names

--- config/ninja5.php
+++ config/ninja5.php
@@ -4,6 +4,8 @@ return [
 
     'billybitcoins' => [
 
+        'twitter_name' => 'Billy',
+
         'inscription_id' => '9d6e3f1c019acaaf89dfbb53432ee654bfa0aa7260848db5e579b5a669a83e38i2',
 
         'inscription_number' => 56255086,
@@ -13,6 +15,8 @@ return [
     ],
 
     'KimiBoyNFT' => [
+
+        'twitter_name' => 'Kimi Boy',
 
         'inscription_id' => '39399e536443446eda3f26ce7038178ec3b69157f38eaed19caea4614aabaf62i95',
 
@@ -24,6 +28,8 @@ return [
 
     'mca_chef' => [
 
+        'twitter_name' => '𝕊𝕔𝕚𝕠𝕟𝕥𝕦𝕥 𓂀 👐🏾🐝',
+
         'inscription_id' => 'e151d62f71ab4e9cfbe1a6ab368db482a393e2a01627b2f9c0e99106cd54cba5i5',
 
         'inscription_number' => 56259592,
@@ -34,6 +40,8 @@ return [
 
     'mvdnbrk' => [
 
+        'twitter_name' => 'Mark',
+
         'inscription_id' => '9d6e3f1c019acaaf89dfbb53432ee654bfa0aa7260848db5e579b5a669a83e38i69',
 
         'inscription_number' => 56255153,
@@ -43,6 +51,8 @@ return [
     ],
 
     'sjen_anoubis' => [
+
+        'twitter_name' => 'Sjen_Anoubis',
 
         'inscription_id' => '650fe4b0abc458836ef869906afbed7c306b1f948a4b3e9412812a7f83900033i88',
 
