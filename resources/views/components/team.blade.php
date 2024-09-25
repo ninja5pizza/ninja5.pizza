901416124200aafb43f45bf86dcc86f3e104b65f
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-25 08:41:34 +0200

    Convert to section

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -1,13 +1,8 @@
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+<section class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="mx-auto max-w-2xl">
         <h2 class="mt-24 text-3xl text-white font-bold">
             TEAM
         </h2>
-    </div>
-</div>
-
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div class="mx-auto max-w-2xl">
         <div class="flex flex-col md:flex-row mt-8 md:space-x-10 text-white">
             @foreach(config('ninja5') as $key => $value)
             <div class="flex flex-col mt-4 md:mt-0">
@@ -24,4 +19,4 @@
             @endforeach
         </div>
     </div>
-</div>
+</section>
