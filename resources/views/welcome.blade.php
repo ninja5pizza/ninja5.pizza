944e55fc8246b375d7b79c13a37d9ddd95c3193e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-25 08:22:25 +0200

    Extract team section to view component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,33 +19,7 @@
         </div>
     </div>
 
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl">
-            <h2 class="mt-24 text-3xl text-white font-bold">
-                TEAM
-            </h2>
-        </div>
-    </div>
-
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl">
-            <div class="flex flex-col md:flex-row mt-8 md:space-x-10 text-white">
-                @foreach(config('ninja5') as $key => $value)
-                <div class="flex flex-col mt-4 md:mt-0">
-                    <a
-                        href="{{ route('profile', ['handle' => $key]) }}"
-                        class="hover:text-orange-100"
-                    >
-                        @svg('ninjas.'.$value['pizza_ninjas_number'], 'w-24 border rounded-lg')
-                        <span class="mt-1 text-xs">
-                            {{ $key }}
-                        </span>
-                    </a>
-                </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
+    <x-team/>
 
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="mx-auto max-w-2xl">
