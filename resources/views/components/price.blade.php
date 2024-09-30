273abd981603cbaf5504d3217d43f419fc1c8bdb
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-30 14:29:48 +0200

    Make price look better on small devices

--- resources/views/components/price.blade.php
+++ resources/views/components/price.blade.php
@@ -1,7 +1,7 @@
 @if(Cache::has('bitcoin_price') && Cache::has('ordinals_collection_stats_pizza-ninjas'))
 <section class="bg-neutral-900">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-center mx-auto max-w-3xl space-x-2 py-1 text-xs font-medium leading-6 text-neutral-400">
+        <div class="flex flex-col md:flex-row items-center justify-center mx-auto max-w-3xl space-x-2 py-1 text-xs font-medium leading-6 text-neutral-400">
             <div class="flex space-x-2">
                 <dt>
                     BITCOIN PRICE
@@ -10,7 +10,7 @@
                     {{ Number::currency(Cache::get('bitcoin_price'), in: 'USD') }}
                 </dd>
             </div>
-            <div class="h-4 w-px bg-neutral-600"></div>
+            <div class="hidden md:block h-4 w-px bg-neutral-600"></div>
             <div class="flex space-x-2">
                 <dt>
                     <a href="https://magiceden.io/ordinals/marketplace/pizza-ninjas" target="_blank">
