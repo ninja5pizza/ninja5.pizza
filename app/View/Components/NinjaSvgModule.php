b8c8061ad56dee6751052d17f76a9b40e30dfc4d
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-06 16:46:57 +0200

    Add method to access the `innerSvgContent`

--- app/View/Components/NinjaSvgModule.php
+++ app/View/Components/NinjaSvgModule.php
@@ -224,6 +224,11 @@ class NinjaSvgModule extends Component implements Htmlable
         return PHP_EOL.'</svg>';
     }
 
+    public function innerSvgContent(): string
+    {
+        return $this->innerSvgContent;
+    }
+
     public function styleElement(): string
     {
         return $this->styleElement;
