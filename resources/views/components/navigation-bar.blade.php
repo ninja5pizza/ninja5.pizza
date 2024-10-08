082d6314a8a275551be6a49981c6066b8c9066f7
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-08 14:04:29 +0200

    Add link to collection page

--- resources/views/components/navigation-bar.blade.php
+++ resources/views/components/navigation-bar.blade.php
@@ -6,12 +6,22 @@
     <nav class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-2">
         <div class="mx-auto max-w-2xl">
             <div class="flex justify-between items-center text-orange-600 leading-6 text-sm">
-                <a
-                    class="hover:text-orange-400"
-                    href="{{ route('home') }}"
-                >
-                    <x-icon-home class="w-6 h-6"/>
-                </a>
+                <div class="flex">
+                    <a
+                        class="hover:text-orange-400"
+                        href="{{ route('home') }}"
+                    >
+                        <x-icon-home class="w-6 h-6"/>
+                    </a>
+                    @if(Route::currentRouteName() !== 'collection')
+                    <a
+                        class="pl-4 hover:text-orange-400"
+                        href="{{ route('collection') }}"
+                    >
+                        collection
+                    </a>
+                    @endif
+                </div>
                 <a
                     class="hover:text-orange-400"
                     href="{{ route('home') }}"
