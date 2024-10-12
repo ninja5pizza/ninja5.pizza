948f55c6b238b4d01d80ff6e2d4099c93cafd4cf
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-12 12:35:30 +0200

    Add stickers config file

---
+++ config/stickers.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    [
+        'label' => 'GM',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi6',
+    ],
+    [
+        'label' => 'Yes',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi10',
+    ],
+    [
+        'label' => 'Fud',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi5',
+    ],
+    [
+        'label' => 'Heart',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi7',
+    ],
+    [
+        'label' => 'Mic',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi8',
+    ],
+    [
+        'label' => 'Wen',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi9',
+    ],
+    [
+        'label' => 'Awesome (CN)',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi0',
+    ],
+    [
+        'label' => 'GM (CN)',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi1',
+    ],
+    [
+        'label' => 'Hello (CN)',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi2',
+    ],
+    [
+        'label' => 'Thanks (CN)',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi3',
+    ],
+    [
+        'label' => 'Very Good (CN)',
+        'inscription_id' => 'beb19efd6c83ced18823f37ffa26d6bf83c85c8372f40725a67af2d90d8b729fi4',
+    ],
+];
