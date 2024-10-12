da4ecaf2daeea48214aee57e2b14635c49ba980e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-12 16:48:46 +0200

    Create holiday-traits.php

---
+++ config/holiday-traits.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'label' => 'new_years_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i5',
+    ],
+    [
+        'label' => 'chinese_new_year',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i1',
+    ],
+    [
+        'label' => 'valentines_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i9',
+    ],
+    [
+        'label' => 'st_patricks_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i8'
+    ],
+    [
+        'label' => 'easter_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i3',
+    ],
+    [
+        'label' => 'april_fools_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i0',
+    ],
+    [
+        'label' => 'bitcoin_pizza_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i7',
+    ],
+    [
+        'label' => 'ordinals_birthday',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i6',
+    ],
+    [
+        'label' => 'halloween',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i4',
+    ],
+    [
+        'label' => 'christmas_day',
+        'inscription_id' => 'c4c41f4d2bc7ea34edf9cd9c652253802f21746c5859932954faf9d065b67d95i2'
+    ],
+];
