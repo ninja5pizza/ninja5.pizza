d385597e8df1d507e6f7d7a43f7c6e6223dda1ea
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-15 09:11:16 +0200

    Refactor

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -2,24 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Str;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ContentController extends Controller
 {
     protected string $file_name;
 
-    public function __invoke(string $inscription_id)
-        {
-            $this->file_name = Str::of($inscription_id)
-                ->append('.svg')
-                ->toString();
+    public function __invoke(string $inscription_id): Response
+    {
+        $this->file_name = Str::of($inscription_id)
+            ->append('.svg')
+            ->toString();
 
-            if (! Storage::disk('ninja_components')->exists($this->file_name)) {
-                abort(404);
-            }
+        if (! Storage::disk('ninja_components')->exists($this->file_name)) {
+            abort(404);
+        }
 
-            return Storage::disk('ninja_components')
-                ->get($this->file_name);
+        $content = Storage::disk('ninja_components')
+            ->get($this->file_name);
+
+        return response($content, 200, [
+            'Content-Type' => 'image/svg+xml',
+        ]);
     }
 }
