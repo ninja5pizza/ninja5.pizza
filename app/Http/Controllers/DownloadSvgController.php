7e8fbacf596ce707a79842086c7b5a14eae4e788
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-15 09:36:15 +0200

    Check if the file exists

--- app/Http/Controllers/DownloadSvgController.php
+++ app/Http/Controllers/DownloadSvgController.php
@@ -8,12 +8,32 @@ use Illuminate\Support\Str;
 
 class DownloadSvgController extends Controller
 {
+    protected $file_name;
+
+    protected $download_file_name;
+
     public function __invoke(Inscription $inscription)
     {
-        return Storage::disk('ninjas')->download(
-            $inscription->getInternalCollectionId().'.svg',
-            Str::of($inscription->name)->slug()->append('.svg')->toString(),
-            ['Content-Type' => 'image/svg+xml']
-        );
+        $this->file_name = Str::of($inscription->getInternalCollectionId())
+            ->append('.svg')
+            ->toString();
+
+        $this->download_file_name = Str::of($inscription->name)
+            ->slug()
+            ->append('.svg')
+            ->toString();
+
+        if (! Storage::disk('ninjas')->exists($this->file_name)) {
+            abort(404);
+        }
+
+        return Storage::disk('ninjas')
+            ->download(
+                $this->file_name,
+                $this->download_file_name,
+                [
+                    'Content-Type' => 'image/svg+xml',
+                ]
+            );
     }
 }
