30b44544738e9e32a9828a6545d6d7ded9cdc814
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-05 12:01:17 +0100

    Add animations by Moodz

--- app/Models/Inscription.php
+++ app/Models/Inscription.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasMoodzAnimations;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
@@ -10,7 +11,7 @@ use Illuminate\Support\Str;
 
 class Inscription extends Model
 {
-    use HasFactory;
+    use HasFactory, HasMoodzAnimations;
 
     protected $fillable = [
         'inscription_id',
