94d5e59a54397d72dd2c72920c90b2d2097fea64
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-15 15:36:22 +0100

    Make footer look better on smaller devices

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,33 +1,33 @@
-<footer class="my-16 border-t border-orange-200 pt-8 sm:mt-20 lg:mt-24" aria-labelledby="footer-heading">
+<footer class="my-16 border-t border-orange-200 pt-8 sm:mt-20 lg:mt-24 text-xs" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="flex flex-col md:flex-row items-center justify-center space-x-4 text-white">
-        <div class="mt-4 flex space-x-4">
+        <div class="flex flex-col md:flex-row mt-4 space-x-0 md:space-x-4 items-center justify-center">
             <p>Proudly hosted with</p>
             <a
                 href="https://m.do.co/c/7a24c68b1e6d"
-                class="hover:text-orange-100"
+                class="mt-4 md:mt-0 hover:text-orange-100"
                 target="_blank"
                 rel="noopener"
             >
                 <span class="sr-only">Digital Ocean</span>
-                <x-icon-digitalocean class="h-6" />
+                <x-icon-digitalocean class="h-6 md:h-4"/>
             </a>
         </div>
         <span class="mt-4 hidden md:inline"> | </span>
-        <div class="mt-4 flex space-x-4">
+        <div class="flex flex-col md:flex-row mt-4 space-x-0 md:space-x-4 items-center justify-center">
             <p>privacy-first insights with</p>
             <a
                 href="https://usefathom.com/ref/FI15PB"
-                class="hover:text-orange-100"
+                class="mt-4 md:mt-0 hover:text-orange-100"
                 target="_blank"
                 rel="noopener"
             >
                 <span class="sr-only">Fathom Analytics</span>
-                <x-icon-fathom class="h-6" />
+                <x-icon-fathom class="h-8 md:h-6"/>
             </a>
         </div>
     </div>
-    <p class="mt-12 text-center text-xs leading-5 text-orange-100">
+    <p class="mt-6 text-center leading-5 text-orange-100">
         &copy; 2024 {{ config('app.name') }}. All rights reserved.
     </p>
 </footer>
