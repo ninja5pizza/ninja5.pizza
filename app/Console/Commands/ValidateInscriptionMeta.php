75e375957887e68b0f454f90226d4c981f870aa1
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-19 13:21:37 +0100

    Add command to validate meta data

---
+++ app/Console/Commands/ValidateInscriptionMeta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ValidateInscriptionMeta extends Command
+{
+    protected $signature = 'ninja:validate-meta';
+
+    protected $description = 'Validate Ninja meta data for missing values.';
+
+    public function handle()
+    {
+        //
+    }
+}
