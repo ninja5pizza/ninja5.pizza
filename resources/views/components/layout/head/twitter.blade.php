6b76e9214068d50e412c50eb8e245811ce5e4757
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-30 15:55:09 +0100

    update title

--- resources/views/components/layout/head/twitter.blade.php
+++ resources/views/components/layout/head/twitter.blade.php
@@ -1,3 +1,3 @@
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="{{ config('app.name') }}">
+<meta name="twitter:title" content="NINJA5 | PIZZA NINJAS">
 <meta name="twitter:image" content="{{ asset('images/240902-twitter-card-large.png') }}">
--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+        <title>NINJA5 | PIZZA NINJAS</title>
 
         <link rel="preconnect" href="https://rsms.me/">
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
