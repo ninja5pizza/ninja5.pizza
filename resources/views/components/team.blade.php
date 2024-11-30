55e64823381ec439aacfda8067e72b0a1084bf46
Mark van den Broek <mvdnbrk@protonmail.com>
2024-11-30 15:52:15 +0100

    change wording

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col mx-auto max-w-2xl">
         <div class="">
         <h2 class="flex justify-center md:justify-normal mt-24 text-3xl text-white font-bold">
-            TEAM
+            PIZZA NINJAS CREATOR BOOTCAMP TEAM
         </h2>
         </div>
         <div class="flex flex-col md:flex-row mt-8 md:space-x-10 text-white">
