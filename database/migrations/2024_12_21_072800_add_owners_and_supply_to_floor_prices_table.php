fe27922e2898a2f79822e5472beb08924ad164c0
Mark van den Broek <mvdnbrk@protonmail.com>
2024-12-21 08:34:16 +0100

    add listed column

--- database/migrations/2024_12_21_072800_add_owners_and_supply_to_floor_prices_table.php
+++ database/migrations/2024_12_21_072800_add_owners_and_supply_to_floor_prices_table.php
@@ -11,13 +11,18 @@ return new class extends Migration
         Schema::table('floor_prices', function (Blueprint $table) {
             $table->integer('owners')->unsigned()->nullable()->after('symbol');
             $table->integer('supply')->unsigned()->nullable()->after('owners');
+            $table->integer('listed')->unsigned()->nullable()->after('supply');
         });
     }
 
     public function down(): void
     {
         Schema::table('floor_prices', function (Blueprint $table) {
-            $table->dropColumn(['owners', 'supply']);
+            $table->dropColumn([
+                'owners',
+                'supply',
+                'listed',
+            ]);
         });
     }
 };
