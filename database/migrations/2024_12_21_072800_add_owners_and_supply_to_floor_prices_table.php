9534a4895032cb6d80a883f1019194f21361ac80
Mark van den Broek <mvdnbrk@protonmail.com>
2024-12-21 08:31:22 +0100

    Add migration to add columns owners and supply

---
+++ database/migrations/2024_12_21_072800_add_owners_and_supply_to_floor_prices_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('floor_prices', function (Blueprint $table) {
+            $table->integer('owners')->unsigned()->nullable()->after('symbol');
+            $table->integer('supply')->unsigned()->nullable()->after('owners');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('floor_prices', function (Blueprint $table) {
+            $table->dropColumn(['owners', 'supply']);
+        });
+    }
+};
