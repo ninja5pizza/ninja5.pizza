9cdb4e9bdd8ea04881c4f0319854cb754ba72756
Mark van den Broek <mvdnbrk@protonmail.com>
2024-12-22 12:43:08 +0100

    Add custom base url for Vite

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
             },
         }),
     ],
+    base: process.env.VITE_BASE_URL || '/',
     resolve: {
         alias: {
             'vue': 'vue/dist/vue.esm-bundler.js'
