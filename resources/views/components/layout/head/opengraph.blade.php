bd982a5e1f3493909ba1853d93c72e28bdcb00ab
Mark van den Broek <mvdnbrk@protonmail.com>
2024-12-22 13:05:21 +0100

    Add `cdn_asset` helper

--- resources/views/components/layout/head/opengraph.blade.php
+++ resources/views/components/layout/head/opengraph.blade.php
@@ -2,4 +2,4 @@
 <meta property="og:type" content="website">
 <meta property="og:title" content="NINJA5 | PIZZA NINJAS">
 <meta property="og:description" content="We are the NINJA5 from the Pizza Ninjas Bootcamp 2024">
-<meta property="og:image" content="{{ secure_asset('opengraph/ninja5/default-twitter-card-large.png') }}">
+<meta property="og:image" content="{{ cdn_asset('opengraph/ninja5/default-twitter-card-large.png') }}">
