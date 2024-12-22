5e10373146bc0c540275af55a7cad7a73aa9a4c0
Mark van den Broek <mvdnbrk@protonmail.com>
2024-12-22 13:12:57 +0100

    Update `twitter:image` content

--- resources/views/components/layout/head/twitter.blade.php
+++ resources/views/components/layout/head/twitter.blade.php
@@ -1,3 +1,3 @@
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="NINJA5 | PIZZA NINJAS">
-<meta name="twitter:image" content="{{ asset('images/240902-twitter-card-large.png') }}">
+<meta name="twitter:image" content="{{ cdn_asset('opengraph/ninja5/default-twitter-card-large.png') }}">
