9343d87e00a4027ed26fb1250b97708932c26447
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-03 11:15:17 +0100

    add return type

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Inscription;
+use Illuminate\View\View;
 
 class InscriptionController extends Controller
 {
-    public function __invoke(Inscription $inscription)
+    public function __invoke(Inscription $inscription): View
     {
         return view('inscription', [
             'inscription' => $inscription,
