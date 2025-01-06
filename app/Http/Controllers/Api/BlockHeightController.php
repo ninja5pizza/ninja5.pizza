4c9f4378152a919fecdec44cf97804e8dc899972
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-06 16:10:23 +0100

    update the response

--- app/Http/Controllers/Api/BlockHeightController.php
+++ app/Http/Controllers/Api/BlockHeightController.php
@@ -4,14 +4,32 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 
 class BlockHeightController extends Controller
 {
     public function __invoke(): JsonResponse
     {
-        return response()->json(
+        $data = Collection::make(
             Cache::get('bitcoin_block_height', default: [])
-        );
+        )
+            ->only([
+                'name',
+                'height',
+                'time',
+            ])
+            ->transform(function ($item) {
+                if (isset($item['time'])) {
+                    $datetime = Carbon::parse($item['time']);
+                    $item['time'] = $datetime->timestamp;
+                }
+
+                return $item;
+            })
+            ->toJson();
+
+        return response()->json($data);
     }
 }
