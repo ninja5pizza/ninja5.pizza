1b5e892dac092d916d5d9ec83eef02c452e0bd7c
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-12 16:44:26 +0100

    Merge migrations

--- database/migrations/2024_11_26_115953_create_floor_prices_table.php
+++ database/migrations/2024_11_26_115953_create_floor_prices_table.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('floor_prices', function (Blueprint $table) {
             $table->id();
             $table->string('symbol')->default('pizza-ninjas');
+            $table->integer('owners')->unsigned()->nullable();
+            $table->integer('supply')->unsigned()->nullable();
+            $table->integer('listed')->unsigned()->nullable();
             $table->double('price_in_sats')->unsigned();
             $table->timestamp('created_at');
         });
