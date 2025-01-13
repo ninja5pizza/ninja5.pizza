145deea0f582c5dd1826476a1e0047d1775cce75
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-13 08:21:39 +0100

    Merge migrations

--- database/migrations/2024_09_09_105053_create_inscriptions_table.php
+++ database/migrations/2024_09_09_105053_create_inscriptions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->string('inscription_id')->unique();
             $table->string('name')->nullable();
             $table->json('meta')->nullable();
+            $table->unsignedMediumInteger('created_at_block')->nullable();
             $table->timestamp('created_at');
         });
     }
