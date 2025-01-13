3559152eb0145775cb0a2dab57d5bb931ba509f0
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-13 10:10:55 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             OrdinalsCollectionSeeder::class,
             PizzaNinjasSeeder::class,
+            PizzaPetsPulverizerSeeder::class,
         ]);
     }
 }
