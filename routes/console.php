6ef162fbc7259335f9fc93c81afdcf2f2f0e4260
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-13 20:27:12 +0100

    Disable blockheight caching

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('bitcoin:cache-height')->everyMinute();
+// Schedule::command('bitcoin:cache-height')->everyMinute();
 Schedule::command('bitcoin:cache-price')->everyFifteenMinutes();
 Schedule::command('ninja:cache-stats')->everyFifteenMinutes();
 Schedule::command('pets:cache-stats')->everyFifteenMinutes();
