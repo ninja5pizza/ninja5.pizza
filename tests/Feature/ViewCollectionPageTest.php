ad59d314de2dfd58a94cd63334cb45a276e6fdaa
Mark van den Broek <mvdnbrk@protonmail.com>
2026-01-12 12:20:05 +0100

    fix test `/collection` page will redirect to `pizza.ninja`

--- tests/Feature/ViewCollectionPageTest.php
+++ tests/Feature/ViewCollectionPageTest.php
@@ -15,34 +15,39 @@ class ViewCollectionPageTest extends TestCase
     #[Test]
     public function a_request_to_the_collection_page_should_return_a_200_status_code(): void
     {
-        $this->get('/collection')->assertOk()->assertViewIs('collection');
+        $this->get('/collection')
+            ->assertRedirect('https://pizza.ninja/collection');
+
+        // $this->get('/collection')
+        //     ->assertOk()
+        //     ->assertViewIs('collection');
     }
 
-    #[Test]
-    public function only_inscriptions_from_pizza_ninjas_collection_are_shown(): void
-    {
-        $pizzaNinjasCollection = OrdinalsCollection::factory()->create([
-            'slug' => 'pizza-ninjas',
-        ]);
+    // #[Test]
+    // public function only_inscriptions_from_pizza_ninjas_collection_are_shown(): void
+    // {
+    //     $pizzaNinjasCollection = OrdinalsCollection::factory()->create([
+    //         'slug' => 'pizza-ninjas',
+    //     ]);
 
-        $pizzaNinjasInscription = Inscription::factory()->create([
-            'collection_id' => $pizzaNinjasCollection->id,
-            'name' => 'Pizza Ninjas #1',
-        ]);
+    //     $pizzaNinjasInscription = Inscription::factory()->create([
+    //         'collection_id' => $pizzaNinjasCollection->id,
+    //         'name' => 'Pizza Ninjas #1',
+    //     ]);
 
-        $otherCollection = OrdinalsCollection::factory()->create([
-            'slug' => 'some-other-collection',
-        ]);
+    //     $otherCollection = OrdinalsCollection::factory()->create([
+    //         'slug' => 'some-other-collection',
+    //     ]);
 
-        $otherInscription = Inscription::factory()->create([
-            'collection_id' => $otherCollection->id,
-            'name' => 'Other Collection Item',
-        ]);
+    //     $otherInscription = Inscription::factory()->create([
+    //         'collection_id' => $otherCollection->id,
+    //         'name' => 'Other Collection Item',
+    //     ]);
 
-        $response = $this->get('/collection');
+    //     $response = $this->get('/collection');
 
-        $response->assertOk();
-        $response->assertSee($pizzaNinjasInscription->name);
-        $response->assertDontSee($otherInscription->name);
-    }
+    //     $response->assertOk();
+    //     $response->assertSee($pizzaNinjasInscription->name);
+    //     $response->assertDontSee($otherInscription->name);
+    // }
 }
